8689060c078fc10dccc839be78f18a5480c66227
Madeline Coles <71307284+maddycoles@users.noreply.github.com>
2020-12-25 01:17:29 -0800

    Add files via upload

---
+++ rockpaperscissors.py
@@ -0,0 +1,38 @@
+import random
+
+while True:
+    user_choice = input("Welcome to Rock, Paper, Scissors!")
+
+    random_num = random.randint(0,2)
+
+    if random_num == 0:
+        cpu_choice = "rock"
+    if random_num == 1:
+        cpu_choice = "paper"
+    if random_num == 2:
+        cpu_choice = "scissors"
+        
+    print( "Your Choice:", user_choice )
+    print( "Computer's Choice:", cpu_choice)
+
+    if user_choice == "rock":
+        if cpu_choice == "rock":
+            print("It's a tie!")
+        elif cpu_choice == "paper":
+            print("You lost!")
+        else:
+            print("You win!")
+    if user_choice == "paper":
+        if cpu_choice == "paper":
+            print("It's a tie!")
+        elif cpu_choice == "scissors":
+            print("You lost!")
+        else:
+            print("You win!")
+    if user_choice == "scissors":
+        if cpu_choice == "scissors":
+            print("It's a tie!")
+        elif cpu_choice == "rock":
+            print("You lost!")
+        else:
+            print("You win!")
